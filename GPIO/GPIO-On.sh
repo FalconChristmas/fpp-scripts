d5f546a9ed48efcf2a7590df67e883778390f0de
Chris Pinkham <cpinkham@mythtv.org>
2014-09-20 20:49:53 -0700

    Add sample scripts for turning On/Off GPIO Outputs.

---
+++ GPIO/GPIO-On.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+###########################################################
+# GPIO-OutputOn.sh - Turn on one of the Pi's GPIO Outputs #
+###########################################################
+
+# GPIO Pin # on the Pi.  These are numbered 0-7 on the main
+# 26-pin header on a A & B.  A model B+ has more available.
+# For the PiFace, use 200-207.
+PIN="0"
+
+# Make sure the pin is setup for output.
+/usr/local/bin/gpio mode ${PIN} out
+
+# Turn on the pin
+/usr/local/bin/gpio write ${PIN} 1
+
