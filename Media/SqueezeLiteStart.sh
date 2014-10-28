94b501ec5a5c9ea1753fd9b16de180745660da59
Chris Pinkham <cpinkham@mythtv.org>
2014-10-28 09:39:44 -0700

    Testing

--- Media/SqueezeLiteStart.sh
+++ Media/SqueezeLiteStart.sh
@@ -8,9 +8,13 @@
 # cd /usr/bin
 # sudo wget http://squeezelite-downloads.googlecode.com/git/squeezelite-armv6hf
 # sudo chmod 755 squeezelite-armv6hf
-#############################################
+##############################################################################
 # Once you have installed squeezelite, edit the 'SERVER' variable below to
 # contain the IP address or hostname of your LMS server.
+#############################################
+# Script Actions
+# InstallAction: sudo wget -O /usr/bin/squeezelite-armv6hf http://squeezelite-downloads.googlecode.com/git/squeezelite-armv6hf
+# InstallAction: sudo chmod 755 /usr/bin/squeezelite-armv6hf
 ##############################################################################
 
 SERVER="192.168.1.1"
