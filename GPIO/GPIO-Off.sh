fdc5773a3e49aa3b15299d94687250c6113c0c19
Chris Pinkham <cpinkham@mythtv.org>
2015-05-27 19:42:56 -0700

    Bugfix for PiFace boards

--- GPIO/GPIO-Off.sh
+++ GPIO/GPIO-Off.sh
@@ -8,9 +8,15 @@
 # For the PiFace, use 200-207.
 PIN="0"
 
+OPTS=""
+if [ ${PIN} -gt 200 ]
+then
+	OPTS="-p"
+fi
+
 # Make sure the pin is setup for output.
-/usr/local/bin/gpio mode ${PIN} out
+/usr/local/bin/gpio ${OPTS} mode ${PIN} out
 
 # Turn off the pin
-/usr/local/bin/gpio write ${PIN} 0
+/usr/local/bin/gpio ${OPTS} write ${PIN} 0
 
