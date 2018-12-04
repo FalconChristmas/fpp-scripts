8652f93b9e3f626f12a93c5efe6dddc399b0cd2e
Chris Pinkham <cpinkham@bc2va.org>
2018-12-04 07:26:42 -0800

    Add 'boot' section and additional comment about run frequency in all sections

--- Control/UserCallbackHook.sh
+++ Control/UserCallbackHook.sh
@@ -4,30 +4,42 @@
 ################################################################################
 
 case $1 in
+	boot)
+		######################################################################
+		# boot is executed at OS boot time before the network is initialized.
+		# This section runs once per system boot.
+		######################################################################
+		# put your commands here
+		;;
+
 	preStart)
 		######################################################################
-		# preStart is executed before plugin preStarts and before fppd startup
+		# preStart is executed before plugin preStarts and before fppd startup.
+		# This section runs every time fppd is started
 		######################################################################
 		# put your commands here
 		;;
 
 	postStart)
 		######################################################################
-		# postStart is executed after fppd and plugin postStarts are run
+		# postStart is executed after fppd and plugin postStarts are run.
+		# This section runs every time fppd is started
 		######################################################################
 		# put your commands here
 		;;
 
 	preStop)
 		######################################################################
-		# preStop is executed before plugin preStop and run and fppd is stopped
+		# preStop is executed before plugin preStop and run and fppd is stopped.
+		# This section runs every time fppd is started
 		######################################################################
 		# put your commands here
 		;;
 
 	postStop)
 		######################################################################
-		# postStop is executed after the plugin postStops are fppd shutdown
+		# postStop is executed after the plugin postStops are fppd shutdown.
+		# This section runs every time fppd is started
 		######################################################################
 		# put your commands here
 		;;
