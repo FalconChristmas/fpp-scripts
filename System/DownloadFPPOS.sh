e3a439f541aff21f7fb19aa07d5d3bb8b733ad99
Scott Hanson <scooter_seh@yahoo.com>
2020-11-01 08:55:41 -0500

    added optional parameter to regex for bugfix releases

--- System/DownloadFPPOS.sh
+++ System/DownloadFPPOS.sh
@@ -18,7 +18,7 @@ echo "Image Type: $TYPE"
 echo "Download the image"
 
 LOCATION=$(curl -s https://api.github.com/repos/FalconChristmas/fpp/releases/latest \
-| grep "browser_download_url.*$TYPE-v[0-9].[0-9].fppos" \
+| grep "browser_download_url.*$TYPE-v[0-9].[0-9].\?[0-9]\?.fppos" \
 | cut -d ":" -f 2,3 \
 | tr -d \" \
 | tr -d [:space:] \
